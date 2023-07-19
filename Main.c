b0c1207385494500e6b396a82bdabf1b09e855b8
GoranVujnovic1985 <goran.vujnovic@nis.rs>
2023-07-19 10:32:39 +0200

    using Strtok

--- Main.c
+++ Main.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main()
 {
 	FILE *file;
-	char line[10];
+	char line[100];
+
+    int brojac = 0;
 
 	file = fopen("Format_Imenika.txt", "r");
 
 	  while (fgets(line, sizeof(line), file))
 	  {
-	           printf("%s", line);
+		  brojac ++;
+
+		  char* ime = strtok(line, "|");
+		  char* prezime = strtok(NULL, "|");
+		  char* broj = strtok(NULL, "\n");
+
+	      printf("%d) Ime: %s, Prezime: %s, Broj_telefona: %s\n\n", brojac, ime, prezime, broj);
+
 	  }
 
 	   fclose(file);
